9485ba1f82f46c7d5cd23c3ca9170e103b2215f5
victorjambo <victorjambo@live.com>
2023-09-11 14:32:23 +0300

    resume page

---
+++ apps/v2/src/app/resume/[receiver]/page.tsx
@@ -0,0 +1,11 @@
+import { Resume } from "ui";
+
+const ResumeView: React.FC = () => {
+  return (
+    <div className="w-full h-screen">
+      <Resume />
+    </div>
+  );
+};
+
+export default ResumeView;
---
+++ packages/ui/src/resume.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export function Resume(): JSX.Element {
+  return (
+    <iframe
+      src="https://drive.google.com/file/d/16hOXWfiA_YJvSBZZd9i680P5ukJRte71/preview"
+      allow="autoplay"
+      className="w-full h-screen"
+    ></iframe>
+  );
+}
