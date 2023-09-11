d46e7af7c74b9c40d5829f2d81fa1dca90cb2e0c
victorjambo <victorjambo@live.com>
2023-09-11 04:52:39 +0300

    page resume

---
+++ apps/v2/src/app/resume/page.tsx
@@ -0,0 +1,12 @@
+const ResumeView: React.FC = () => {
+  return (
+    <div>
+      <iframe
+        className="w-full h-screen"
+        src="/victor-mutai-resume.pdf"
+      ></iframe>
+    </div>
+  );
+};
+
+export default ResumeView;
