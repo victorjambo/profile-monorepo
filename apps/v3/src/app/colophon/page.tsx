ea63ff660f9e10fc0724037df931009a40e71299
victorjambo <victorjambo@live.com>
2023-09-13 01:44:19 +0300

    colophon

--- apps/v3/src/app/colophon/page.tsx
+++ apps/v3/src/app/colophon/page.tsx
@@ -1,10 +1,48 @@
-import { NotImplemented } from "ui";
+import { Laila } from "next/font/google";
+import { NavBack } from "ui";
 import Footer from "../../components/footer";
+import HeaderLinks from "../../components/links/header";
+
+const laila = Laila({
+  subsets: ["latin"],
+  weight: "700",
+});
 
 export default function Page(): JSX.Element {
   return (
-    <NotImplemented>
-      <Footer />
-    </NotImplemented>
+    <main className="w-full relative">
+      <div className="h-screen p-6 md:p-11">
+        <NavBack />
+        <HeaderLinks />
+        <div className="max-w-[65ch]">
+          <div
+            className={`${laila.className} dark:text-white text-xl font-bold pb-5`}
+          >
+            colophon
+          </div>
+
+          <div className="py-1.5 pl-4 border-l-4 border-gray-900 dark:border-white">
+            <blockquote>
+              A <i>colophon</i> is a designer word for 'how this site was made'
+            </blockquote>
+          </div>
+
+          <div className="py-5 flex flex-col space-y-5 leading-7">
+            Here are a few of the tools I used to put this site together:
+            <ul className="pl-6">
+              <li>• NextJS</li>
+              <li>• ReactJS</li>
+              <li>• Turborepo</li>
+              <li>• Tailwindcss</li>
+              <li>• Netlify</li>
+              <li>• Vercel</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="absolute bottom-0 w-full">
+        <Footer />
+      </div>
+    </main>
   );
 }
