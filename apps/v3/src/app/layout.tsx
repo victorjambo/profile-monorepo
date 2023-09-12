195b8f5b1c141b63dddb5660792363dce1278a83
victorjambo <victorjambo@live.com>
2023-09-12 21:38:38 +0300

    auto dark mode

--- apps/v3/src/app/layout.tsx
+++ apps/v3/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <Analytics />
 
       <body
