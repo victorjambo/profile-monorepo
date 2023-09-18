532973bc353a63b521d286b3320909bd3f901f2b
victorjambo <victorjambo@live.com>
2023-09-19 02:11:34 +0300

    iframe

--- apps/v2/src/app/resume/[receiver]/page.tsx
+++ apps/v2/src/app/resume/[receiver]/page.tsx
@@ -1,9 +1,12 @@
-import { Resume } from "ui";
-
 export default function ResumeView(): JSX.Element {
   return (
     <div className="w-full h-screen">
-      <Resume />
+      <iframe
+        allow="autoplay"
+        className="w-full h-screen"
+        src="https://drive.google.com/file/d/16hOXWfiA_YJvSBZZd9i680P5ukJRte71/preview"
+        title="resume"
+      />
     </div>
   );
 }
