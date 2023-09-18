8d9ac861090c6c628b86091b8fcb0492927f695a
victorjambo <victorjambo@live.com>
2023-09-18 23:34:11 +0300

    why are you doing this to me

--- apps/v3/src/app/page.tsx
+++ apps/v3/src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Script from "next/script";
 import HeaderLinks from "../components/links/header";
 import Work from "../components/work";
 import Hero from "../components/hero";
@@ -8,7 +9,7 @@ import Projects from "../components/projects";
 export default function Page(): JSX.Element {
   return (
     <>
-      {/* {process.env.NODE_ENV !== "development" && <Script src="/scripts/debugger.js" />} */}
+      {process.env.NODE_ENV !== "development" && <Script src="/scripts/debugger.js" />}
       <main className="w-full">
         <div className="h-screen p-6 md:p-11 grid md:grid-cols-2">
           <div className="md:order-first order-last">
