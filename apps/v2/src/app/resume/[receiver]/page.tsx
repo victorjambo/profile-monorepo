52401957d6b763265a92b3edf224b8f581ff2850
victorjambo <victorjambo@live.com>
2023-09-19 03:07:41 +0300

    debugger

--- apps/v2/src/app/resume/[receiver]/page.tsx
+++ apps/v2/src/app/resume/[receiver]/page.tsx
@@ -2,10 +2,13 @@ export default function ResumeView(): JSX.Element {
   return (
     <div className="w-full h-screen">
       <iframe
-        allow="autoplay"
-        className="w-full h-screen"
-        src="https://drive.google.com/file/d/16hOXWfiA_YJvSBZZd9i680P5ukJRte71/preview"
-        title="resume"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+        frameBorder="0"
+        height="315"
+        src="https://www.youtube.com/embed/qtPSqLTvbDU?si=5_DJiy2kyZg2CDze"
+        title="YouTube video player"
+        width="560"
       />
     </div>
   );
