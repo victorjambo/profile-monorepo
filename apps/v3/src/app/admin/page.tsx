8bbd5421fb9d7d0355667107ae0ce1d4b35e5e3a
victor mutai (github) <victormutaijambo@gmail.com>
2023-09-26 18:45:47 +0300

    rounded

--- apps/v3/src/app/admin/page.tsx
+++ apps/v3/src/app/admin/page.tsx
@@ -1,3 +1,7 @@
 export function Page(): JSX.Element {
-  return <main className="w-full">sign in</main>;
+  return (
+    <main className="w-full">
+      <div className="rounded-xl shadow-md">sign in</div>
+    </main>
+  );
 }
