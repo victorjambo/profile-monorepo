1cd0610a740d1245e0282e6e8a0c7dcbac473720
victorjambo <victorjambo@live.com>
2023-09-26 17:31:10 +0300

    blogs

--- apps/blog/pages/_document.tsx
+++ apps/blog/pages/_document.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  render() {
+  render(): JSX.Element {
     return (
       <Html lang="en">
         <Head />
-        <body className="prose">
+        <body>
           <Main />
           <NextScript />
         </body>
