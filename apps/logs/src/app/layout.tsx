a3843f9a74aa870155a05caf3893901ed172ec9c
victor mutai (github) <victormutaijambo@gmail.com>
2023-09-28 16:43:35 +0300

    logs

---
+++ apps/logs/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import "./globals.css";
+import "ui/styles.css";
+
+import type { Metadata } from "next";
+import { Comfortaa } from "next/font/google";
+import { Analytics } from "ui";
+import { MetadataSEO } from "shared-data";
+
+const comfortaa = Comfortaa({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = MetadataSEO;
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  return (
+    <html lang="en">
+      {process.env.NODE_ENV === "production" && <Analytics />}
+
+      <body
+        className={`${comfortaa.className} light old-text-serket dark:text-slate-300 old-bg-summer dark:bg-gray-900 bg-cover bg-fixed`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
