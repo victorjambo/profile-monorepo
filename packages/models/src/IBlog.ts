7fb9691b7c5692b50d06032b8af1ad2308b1d0fc
victor mutai (github) <victormutaijambo@gmail.com>
2023-10-06 00:06:04 +0300

    chore: sunset models

--- packages/models/src/IBlog.ts
+++
@@ -1,8 +0,0 @@
-export interface IBlog {
-  id: number;
-    title: string;
-    href?: string;
-    description: string;
-    date: string;
-    datetime: string;
-}
--- packages/models/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./IEvent";
-export * from "./INavBack";
-export * from "./ICounter";
-export * from "./blog/page-map";
