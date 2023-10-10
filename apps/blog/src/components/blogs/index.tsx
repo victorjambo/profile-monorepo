cd893dda345d381f12f08c21f08f8359dc7ff9db
victor mutai (github) <victormutaijambo@gmail.com>
2023-10-10 19:50:50 +0300

    feat: ui

--- apps/blog/src/components/blogs/index.tsx
+++ apps/blog/src/components/blogs/index.tsx
@@ -1,4 +1,6 @@
 "use client";
+import Link from "next/link";
+import Image from "next/image";
 import { allBlogs } from "../../../.contentlayer/generated";
 import Blog from "../blog";
 import TableFilter from "../table-filter";
@@ -24,20 +26,47 @@ export default function Blogs({
   });
 
   return (
-    <table className="w-full">
-      <thead>
-        <tr className="pb-5">
-          <th className="pl-3 text-left">title</th>
-          <th className="pr-3 text-right">
-            <TableFilter />
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {blogs.map((blog, idx) => (
-          <Blog blog={blog} idx={idx} key={blog._id} len={blogs.length} />
-        ))}
-      </tbody>
-    </table>
+    <div>
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+        <div className="flex-1 space-y-4">
+          <h1 className="inline-block font-heading text-4xl tracking-tight lg:text-5xl">
+            Blog
+          </h1>
+          <p className="text-xl text-muted-foreground">
+            A blog built using Contentlayer. Posts are written in MDX.
+          </p>
+        </div>
+      </div>
+      <hr className="my-8" />
+      {blogs.length ? (
+        <div className="grid gap-10 sm:grid-cols-2">
+          {blogs.map((blog, index) => (
+            <article
+              className="group relative flex flex-col space-y-2"
+              key={blog._id}
+            >
+              {blog.image ? <Image
+                  alt={blog.title}
+                  className="rounded-md border bg-muted transition-colors h-[225px] object-fill"
+                  height={225}
+                  priority={index <= 1}
+                  src={blog.image}
+                  width={804}
+                /> : null}
+              <h2 className="text-2xl font-extrabold">{blog.title}</h2>
+              {blog.description ? <p className="text-muted-foreground">{blog.description}</p> : null}
+              {blog.date ? <p className="text-sm text-muted-foreground">
+                  {blog.date}
+                </p> : null}
+              <Link className="absolute inset-0" href={blog.slug} >
+                <span className="sr-only">View Article</span>
+              </Link>
+            </article>
+          ))}
+        </div>
+      ) : (
+        <p>No posts published.</p>
+      )}
+    </div>
   );
 }
