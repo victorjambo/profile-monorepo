28c257a26423b304162e9625ad64383e5de06083
victor mutai (github) <victormutaijambo@gmail.com>
2023-10-13 03:18:49 +0300

    feat: hover to show image

--- apps/v3/src/components/projects/index.tsx
+++ apps/v3/src/components/projects/index.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
 import { projects } from "builders";
 import ProjectEvent from "../shared/project-event";
 
 export default function Projects(): JSX.Element {
+  const [currentImg, setCurrentImg] = useState<{
+    title: string;
+    img: string;
+  }>({
+    title: "",
+    img: "",
+  });
+
   return (
     <div className="h-fit px-6 md:px-11 pb-20 mt-10 lg:-mt-16">
       <div className="grid md:grid-cols-2">
@@ -18,12 +27,24 @@ export default function Projects(): JSX.Element {
                   counter={idx + 1}
                   event={project}
                   key={project.title}
+                  setCurrentImg={setCurrentImg}
                 />
               ))}
             </div>
           </div>
         </div>
-        <div />
+
+        <div className="hidden md:block">
+          <div className="py-5">&nbsp;</div>
+          <div>
+            {currentImg.img ? (
+              <img
+                alt={currentImg.title}
+                src={currentImg.img}
+              />
+            ) : null}
+          </div>
+        </div>
       </div>
     </div>
   );
--- apps/v3/src/components/shared/project-event.tsx
+++ apps/v3/src/components/shared/project-event.tsx
@@ -1,16 +1,37 @@
+import type { Dispatch, SetStateAction } from "react";
 import type { Tech } from "builders";
 import type { EventProp } from "../../types";
 
-type Props = EventProp<(typeof Tech)[number]>;
+type Props = EventProp<(typeof Tech)[number]> & {
+  setCurrentImg: Dispatch<
+    SetStateAction<{
+      title: string;
+      img: string;
+    }>
+  >;
+};
 
 export default function ProjectEvent({
-  event: { title, role, link, tech },
+  event: { title, role, link, tech, img },
   counter,
   count,
+  setCurrentImg,
 }: Props): JSX.Element {
   return (
     <div
       className={`${counter === count ? "" : "border-b dark:border-slate-300"}`}
+      onMouseEnter={() => {
+        setCurrentImg({
+          title,
+          img: img ?? "",
+        });
+      }}
+      onMouseLeave={() => {
+        setCurrentImg({
+          title: "",
+          img: "",
+        });
+      }}
     >
       <a
         className="grid grid-row-2 lg:grid-rows-none grid-cols-none lg:grid-cols-3 gap-4 lg:gap-0 py-4"
@@ -18,7 +39,6 @@ export default function ProjectEvent({
         rel="noopener noreferrer"
         target="_blank"
       >
-        
         <div className="font-extrabold">
           <sup className="pr-1">{counter}.</sup>
           <span className="hover:border-b border-vase dark:border-slate-300 border-dotted">
@@ -29,7 +49,6 @@ export default function ProjectEvent({
         <div className="hover:border-b border-vase dark:border-slate-300 border-dotted leading-loose lg:col-span-2 lg:text-right">
           {role}
         </div>
-
       </a>
 
       {tech ? (
