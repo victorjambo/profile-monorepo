bd16d1009456a1adb8f0e773834a736bc48b971c
Victor Mutai <victormutaijambo@gmail.com>
2025-02-27 01:29:54 +0300

    install flowbite

--- apps/v4/tailwind.config.ts
+++ apps/v4/tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
+import flowbite from "flowbite-react/tailwind";
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -14,5 +17,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [typography, flowbite.plugin()],
 } satisfies Config;
